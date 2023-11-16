46d10bc04f37224ec65471a6c1ca9296b0c885eb
Denis Kudia <dkudja@gmail.com>
2023-11-15 17:28:30 -0800

    Threaded consumer

--- colorMusic/consumer.h
+++ colorMusic/consumer.h
@@ -13,15 +13,49 @@
 #define CMUSIC_CONSUMER
 
 #include <cstdint>
+#include "Threaded.h"
 
 namespace cmusic {
 
 using Tp = std::chrono::time_point<std::chrono::system_clock>;
 
-class Consumer {
+/**
+ * @brief
+ *
+ */
+class Consumer : public piutils::Threaded {
 public:
     Consumer() {}
     virtual ~Consumer() {}
+
+    /**
+     * @brief
+     *
+     * @return true
+     * @return false
+     */
+    bool start(){
+        logger::log(logger::LLOG::INFO, "consm", std::string(__func__));
+        return piutils::Threaded::start<Consumer>(this);
+    }
+
+    /**
+     * @brief
+     *
+     */
+    void stop(){
+        logger::log(logger::LLOG::INFO, "consm", std::string(__func__));
+        piutils::Threaded::stop();
+    }
+
+    /**
+     * @brief
+     *
+     */
+    void wait(){
+        logger::log(logger::LLOG::INFO, "consm", std::string(__func__));
+        piutils::Threaded::wait();
+    }
 
     /**
      * @brief
@@ -31,10 +65,21 @@ public:
      */
     virtual void process(const uint32_t* data, const int d_size) = 0;
 
+    /**
+     * @brief
+     *
+     * @return const Tp
+     */
     const Tp processing_start() {
         return std::chrono::system_clock::now();
     }
 
+    /**
+     * @brief
+     *
+     * @param tp_start
+     * @return const std::string
+     */
     const std::string processing_end(const Tp tp_start){
         auto tp_end = std::chrono::system_clock::now();
         return std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(tp_end - tp_start).count());
